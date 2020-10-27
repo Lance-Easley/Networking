bab6fc4bc2a69e1a2b5f1523f0aa13840007f2e0
Lance Easley <leasley21@basecampcodingacademy.org>
2020-10-27 14:39:27 -0500

    Progress (10-27)

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 from _thread import *
 import sys
-from player import Player
+from player import Player, Absent
 import pickle
 
 server = "192.168.1.59"
@@ -17,11 +17,13 @@ except socket.error as e:
 s.listen(9)
 print("Waiting for a connection, Server Started")
 
-players = [
+player_list = [
     Player(0, 0, 40, 60, (255, 0, 0)), Player(100, 0, 40, 60, (0, 0, 255)), Player(200, 0, 40, 60, (0, 255, 0)), 
     Player(0, 100, 40, 60, (255, 0, 0)), Player(100, 100, 40, 60, (0, 0, 255)), Player(200, 100, 40, 60, (0, 255, 0)), 
     Player(0, 200, 40, 60, (255, 0, 0)), Player(100, 200, 40, 60, (0, 0, 255)), Player(200, 200, 40, 60, (0, 255, 0)), 
     ]
+
+players = []
 
 def threaded_client(conn, player):
     conn.send(pickle.dumps(players[player]))
@@ -35,51 +37,55 @@ def threaded_client(conn, player):
                 print("Disconnected")
                 break
             else:
-                if player == 0:
-                    reply = [
-                        players[1], players[2], players[3], players[4], 
-                        players[5], players[6], players[7], players[8]
-                        ] 
-                elif player == 1:
-                    reply = [
-                        players[0], players[2], players[3], players[4], 
-                        players[5], players[6], players[7], players[8]
-                        ]
-                elif player == 2: 
-                    reply = [
-                        players[1], players[0], players[3], players[4], 
-                        players[5], players[6], players[7], players[8]
-                        ]
-                elif player == 3:
-                    reply = [
-                        players[1], players[2], players[0], players[4], 
-                        players[5], players[6], players[7], players[8]
-                        ]
-                elif player == 4:
-                    reply = [
-                        players[1], players[2], players[3], players[0], 
-                        players[5], players[6], players[7], players[8]
-                        ]
-                elif player == 5:
-                    reply = [
-                        players[1], players[2], players[3], players[4], 
-                        players[0], players[6], players[7], players[8]
-                        ]
-                elif player == 6:
-                    reply = [
-                        players[1], players[2], players[3], players[4], 
-                        players[5], players[6], players[7], players[8]
-                        ]
-                elif player == 7:
-                    reply = [
-                        players[1], players[2], players[3], players[4], 
-                        players[5], players[6], players[0], players[8]
-                        ]
-                elif player == 8:
-                    reply = [
-                        players[1], players[2], players[3], players[4], 
-                        players[5], players[6], players[7], players[0]
-                        ]
+                reply = []
+                for p in range(len(players)):
+                    if p != player:
+                        reply.append(players[p])
+                # if player == 0:
+                #     reply = [
+                #         players[1], players[2], players[3], players[4], 
+                #         players[5], players[6], players[7], players[8]
+                #         ] 
+                # elif player == 1:
+                #     reply = [
+                #         players[0], players[2], players[3], players[4], 
+                #         players[5], players[6], players[7], players[8]
+                #         ]
+                # elif player == 2: 
+                #     reply = [
+                #         players[1], players[0], players[3], players[4], 
+                #         players[5], players[6], players[7], players[8]
+                #         ]
+                # elif player == 3:
+                #     reply = [
+                #         players[1], players[2], players[0], players[4], 
+                #         players[5], players[6], players[7], players[8]
+                #         ]
+                # elif player == 4:
+                #     reply = [
+                #         players[1], players[2], players[3], players[0], 
+                #         players[5], players[6], players[7], players[8]
+                #         ]
+                # elif player == 5:
+                #     reply = [
+                #         players[1], players[2], players[3], players[4], 
+                #         players[0], players[6], players[7], players[8]
+                #         ]
+                # elif player == 6:
+                #     reply = [
+                #         players[1], players[2], players[3], players[4], 
+                #         players[5], players[6], players[7], players[8]
+                #         ]
+                # elif player == 7:
+                #     reply = [
+                #         players[1], players[2], players[3], players[4], 
+                #         players[5], players[6], players[0], players[8]
+                #         ]
+                # elif player == 8:
+                #     reply = [
+                #         players[1], players[2], players[3], players[4], 
+                #         players[5], players[6], players[7], players[0]
+                #         ]
 
                 print("Received:", data)
                 print("Sending:", reply)
@@ -93,6 +99,7 @@ def threaded_client(conn, player):
 currentPlayer = 0
 while True:
     conn, addr = s.accept()
+    players.append(player_list[currentPlayer])
     print("Connected to:", addr)
 
     start_new_thread(threaded_client, (conn, currentPlayer))
